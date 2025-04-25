c4e456dd1b504997b55f70d8b9650e99871213de
Jorawar Singh Dham <gogig21@gmail.com>
2025-04-25 18:54:32 -0400

    q1_q1_to_q5

--- q1_vae.py
+++ q1_vae.py
@@ -24,9 +24,9 @@ def log_likelihood_bernoulli(mu, target):
     target = target.view(batch_size, -1)
 
     #TODO: compute log_likelihood_bernoulli
-    raise NotImplementedError
+    ll_bernoulli = target * torch.log(mu) + (1 - target) * torch.log(1 - mu) # shape: (batch_size, input_size)
 
-    return ll_bernoulli
+    return ll_bernoulli.sum(dim=1)
 
 
 def log_likelihood_normal(mu, logvar, z):
@@ -43,11 +43,16 @@ def log_likelihood_normal(mu, logvar, z):
     # init
     batch_size = mu.size(0)
     mu = mu.view(batch_size, -1)
+    d = mu.size(1)
     logvar = logvar.view(batch_size, -1)
     z = z.view(batch_size, -1)
+    
 
+    # Compute squared Euclidean distance between z and mu
+    squared_dist_normal = ((z - mu) ** 2)/torch.exp(logvar) # shape: (batch_size, input_size)
+    
     #TODO: compute log normal
-    raise NotImplementedError
+    ll_normal = -0.5 * (d * math.log(2 * math.pi) + logvar.sum(dim=1) + squared_dist_normal.sum(dim=1)) # shape: (batch_size,)
     
     return ll_normal
 
@@ -66,9 +71,11 @@ def log_mean_exp(y):
     sample_size = y.size(1)
 
     #TODO: compute log_mean_exp
-    raise NotImplementedError
+    a =   y.max(dim=1)[0].unsqueeze(1) # shape: (batch_size, 1)
+    exp_diff_sum = torch.exp(y - a).sum(dim=1)  # shape: (batch_size,)
+    lme = a + torch.log(exp_diff_sum/sample_size) # shape: (batch_size,)
 
-    return lme 
+    return lme
 
 
 def kl_gaussian_gaussian_analytic(mu_q, logvar_q, mu_p, logvar_p):
@@ -91,9 +98,12 @@ def kl_gaussian_gaussian_analytic(mu_q, logvar_q, mu_p, logvar_p):
     logvar_p = logvar_p.view(batch_size, -1)
 
     #TODO: compute kld
-    raise NotImplementedError
+    var_q = torch.exp(logvar_q) # shape: (batch_size, input_size)
+    var_p = torch.exp(logvar_p) # shape: (batch_size, input_size)
+    kl_gg = 0.5 * ( (var_q/var_p) +((mu_p - mu_q) ** 2)/var_p + logvar_p - logvar_q - 1 ) # shape: (batch_size, input_size)
+    
 
-    return kl_gg
+    return kl_gg.sum(dim=1) # shape: (batch_size,)
 
 
 def kl_gaussian_gaussian_mc(mu_q, logvar_q, mu_p, logvar_p, num_samples=1):
@@ -118,6 +128,14 @@ def kl_gaussian_gaussian_mc(mu_q, logvar_q, mu_p, logvar_p, num_samples=1):
     logvar_p = logvar_p.view(batch_size, -1).unsqueeze(1).expand(batch_size, num_samples, input_size)
 
     #TODO: compute kld
-    raise NotImplementedError
+    std_q = torch.exp(logvar_q / 2) # shape: (batch_size, num_samples, input_size)
+    noise = torch.randn_like(std_q) # shape: (batch_size, num_samples, input_size)
+    z = mu_q + std_q * noise # shape: (batch_size, num_samples, input_size)
+    log_q = log_likelihood_normal(mu_q, logvar_q, z)/num_samples # shape: (batch_size,)
+    log_p = log_likelihood_normal(mu_p, logvar_p, z)/num_samples # shape: (batch_size,)
+    kl_mc = log_q - log_p # shape: (batch_size,)
+    
+    
+
 
     return kl_mc
